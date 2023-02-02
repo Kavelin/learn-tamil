b657849dad0c74ffb82694aa688f4c13803a8fc5
Crazexpert <dev.velp@gmail.com>
2023-02-02 15:54:43 +0000

    working now!

--- pages/learn/[level].vue
+++ pages/learn/[level].vue
@@ -1,19 +1,52 @@
 <template>
-
-    <div><h1>hi{{ theLevel }} </h1></div>
-    <div id="gaming">
-      <h1 id="to_type"> {{ to_type }}</h1>
-      <input type="text" v-model="main_input" />
+  <div v-if="!gam">
+    <h1>Level {{ theLevel }}</h1>
+    <button @click="nextPage">Next -></button>
+  </div>
+  <div id="gaming" v-else>
+    <div v-if="!defin">
+      <h1 id="to_type">{{ words[currentWord].word }}</h1>
+      <input type="text" @keypress.enter="check" v-model="mainInput" />
+      <button @click="check">Next -></button>
     </div>
-  </template>
-  
-  <script lang="ts" setup>
-  
-  const route = useRoute();
-  
-  let theLevel = route.params.id;
-  let gam:boolean = false;
-  let main_input:String = "";
-  let to_type:String = "";
-  </script>
-  
+    <div v-else>
+      <h1>{{ words[currentWord].def }}</h1>
+      <button @click="nextWord">Next -></button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const route = useRoute();
+
+interface Word {
+  image?: String;
+  word: String;
+  rom: String; //romanization
+  def: String;
+}
+
+let theLevel = useState("theLevel", () => route.params.level);
+
+let gam = useState("gam", () => false);
+let defin = useState("defin", () => false);
+let mainInput = useState("mainInput", () => "");
+let words = useState("words", () => [
+  { word: "அம்மா", rom: "amma", def: "mom" },
+  { word: "அப்பா", rom: "appa", def: "dad" },
+]);
+let currentWord = useState("currentWord", () => 0);
+let nextPage = () => {
+  //if (pages opofdals jflaj)
+  gam.value = !gam.value;
+}
+let check = () => {
+  if (mainInput.value == words.value[currentWord.value].rom)
+    defin.value = !defin.value;
+  mainInput.value = "";
+};
+let nextWord = () => {
+  defin.value = !defin.value;
+  currentWord.value++;
+};
+</script>
