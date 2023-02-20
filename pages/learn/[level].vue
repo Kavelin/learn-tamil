ff5beb6e097500c9248c4594d066cdef53b13b1d
Crazexpert <dev.velp@gmail.com>
2023-02-20 19:10:22 +0000

    more gifs

--- pages/learn/[level].vue
+++ pages/learn/[level].vue
@@ -176,7 +176,7 @@ let nextWord = async () => {
     if (levels[curLevel.value]) {
       await navigateTo("./" + ++curLevel.value);
       router.go(0);
-    } else await navigateTo("./completed");
+    } else await navigateTo("./completed-page");
   }
 };
 </script>
