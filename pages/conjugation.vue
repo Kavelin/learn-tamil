a37713e17901c39947280adda6d9cc54ada239fc
Crazexpert <dev.velp@gmail.com>
2023-02-25 15:50:28 +0000

    cap sounds works now

--- pages/conjugation.vue
+++ pages/conjugation.vue
@@ -1,3 +1,14 @@
 <template>
-    In progress
+  <form>
+    <label for="lname">{{ verb }}</label
+    ><br />
+    <label>{{ perspective }}</label
+    ><br />
+    <input type="text" id="" name="lname" />
+    <input type="submit" value="Enter" />
+  </form>
 </template>
+<script lang="ts" setup>
+const verb = "";
+const perspective = "";
+</script>
