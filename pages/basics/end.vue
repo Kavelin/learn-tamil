796ec0caca98549f939ce9d7347892decb60220d
Kavelin <34014055+Kavelin@users.noreply.github.com>
2024-09-27 18:50:03 +0000

    sounds work now + slight modifications to lessons and css

--- pages/basics/end.vue
+++ pages/basics/end.vue
@@ -1,3 +1,8 @@
 <template>
-    You finished!
+    Great job! You've learned the alphabet of the Tamil language, as well as a few words. <br>
+    Here are some useful resources to continue your Tamil journey: <br>
+    <a href="https://vilaiyaatu.com">https://vilaiyaatu.com</a><br>
+    <a href="https://www.imaginetamil.com/">https://www.imaginetamil.com/</a> <br>
+    <a href="https://payil.karky.in/">https://payil.karky.in/</a> <br>
+    <a href="https://www.learntamil.com/">https://www.learntamil.com/</a> <br>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,9 @@
   <div id="wrapper">
     <div>
       <span id="title">Learn Tamil!</span>
-      <NuxtLink :to="'/basics/' + learnLevel">Basics -></NuxtLink>
-      <NuxtLink to="/conjugation/">Conjugation -></NuxtLink>
+      <br> <br>
+      <NuxtLink :to="'/basics/' + learnLevel">Basics -> <br> <i> learn the tamil alphabet! </i> </NuxtLink>
+      <a class="disabled" title="under construction!">Conjugation -> <br> <i> learn conjugation of verbs! (under construction)</i> </a>
     </div>
   </div>
 </template>
@@ -21,28 +22,39 @@
 #title {
   font-weight:300;
   font-size:3em;
+  text-align:center;
 }
 a {
   border: 2px solid #adadad;
+  background: #d4d4d4;
   border-radius: 4px;
   color: black;
   text-decoration: none;
-  cursor: default;
-  padding: 8px;
+  padding: 8px 0 8px 8px;
   font-family: "Noto Sans Tamil", sans-serif;
   margin: 8px;
   transition: background 300ms, border-color 300ms, color 300ms;
-  background: #d4d4d4;
-  width: fit-content;
+  width: 100%;
   display: block;
 }
 a:hover {
   background: #adadad;
-  color: #3939d8;
+  color: black;
 }
 a:click {
   background: #353535;
   border-color: #353535;
+}
+a i {
+  font-size:80%;
+  color: rgb(126, 126, 126);
+}
+.disabled {
+  color: rgb(126, 126, 126)!important;
+  background: #d4d4d4!important;
+  cursor: not-allowed;
+  opacity: 0.5;
+  text-decoration: none;
 }
 </style>
 <script setup lang="ts">
